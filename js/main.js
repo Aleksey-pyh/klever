30018bb9332e4ed224c9b2bb2911129b4b7c57d1
Aleksey-pyh <69820361+Aleksey-pyh@users.noreply.github.com>
2020-12-12 23:26:45 +0300

    +

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slidersPic').slick({
+  nextArrow : $('.next'),
+  prevArrow : $('.prev'),
+});
+$('.toolSlider').slick({
+  nextArrow : $('.nextTool'),
+  prevArrow : $('.prevTool'),
+  slidesToShow: 4,
+  slidesToScroll:1,
+});
