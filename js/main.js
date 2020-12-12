8e1d1897c6f7c0e542175ddb62dbd9de21da99ed
Aleksey-pyh <69820361+Aleksey-pyh@users.noreply.github.com>
2020-12-13 00:22:29 +0300

    +

--- js/main.js
+++ js/main.js
@@ -7,4 +7,24 @@ $('.toolSlider').slick({
   prevArrow : $('.prevTool'),
   slidesToShow: 4,
   slidesToScroll:1,
+  responsive: [
+    {
+        breakpoint: 1110,
+        settings: {
+            slidesToShow:3,
+        }
+    },
+    {
+      breakpoint: 800,
+      settings: {
+          slidesToShow:2,
+      }
+  },
+  {
+    breakpoint: 460,
+    settings: {
+        slidesToShow:1,
+    }
+}
+] 
 });
